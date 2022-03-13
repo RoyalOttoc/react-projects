0e1f768cbb2571eaa41a16c5d18f4cd5e8bc9990
Jong Kim <jong.kim8706@gmail.com>
2022-03-14 05:47:10 +1100

    styled component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, {keyframes} from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.backgroundColor};
+  color: ${props => props.theme.textColor};
+`;
+
+const rotationAnimation = keyframes`
+ 0%{
+   transform: rotate(0)
+ }
+ 50%{
+   transform: rotate(180deg);
+   border-radius: 50px;
+
+ }
+ 100%{
+   transform: rotate(0);
+   border-radius: 0px;
+ }
+`;
+
+const Box = styled.div`
+  background-color: ${props => props.bgColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 300px;
+  animation: ${rotationAnimation} 2s linear infinite;
+  span {
+    font-size: 30px;
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Wrapper>
+        <Box bgColor="tomato">
+          <span>Hello World</span>
+        </Box>
+      </Wrapper>
     </div>
   );
 }
